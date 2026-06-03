db5a4d865d69e03552f26bb4c0d2154fddf7991d
jos <jos.clephas@nerium.nl>
2026-06-03 10:19:00 +0200

    Added default for optional variables to avoid pydantic errors on run

--- core/utils/config.py
+++ core/utils/config.py
@@ -34,18 +34,18 @@ class Config(BaseSettings):
     )
 
     blob_storageaccount_enabled: bool
-    blob_storageaccount_uri: str
-    blob_container_input: str
-    blob_container_status: str
+    blob_storageaccount_uri: str = ''
+    blob_container_input: str = ''
+    blob_container_status: str = ''
     blob_container_config: str = 'config'
     blob_storageaccount_sas_enabled: bool
-    blob_storageaccount_sas: str
+    blob_storageaccount_sas: str = ''
     blob_logtable_enabled: bool
     blob_logtable_uri: str
     blob_logtable_name: str
     blob_queue_enabled: bool
-    blob_queue_url: str
-    blob_queue_name: str
+    blob_queue_url: str = ''
+    blob_queue_name: str = ''
     adx_cluster_enabled: bool
     adx_database_name: str
     adx_cluster_uri: str
@@ -58,22 +58,22 @@ class Config(BaseSettings):
     adx_ingestion_batching_max_items: int
     adx_ingestion_batching_max_size_mb: int
     keyvault_enabled: bool
-    keyvault_url: str
-    keyvault_passwordlocation: str
+    keyvault_url: str = ''
+    keyvault_passwordlocation: str = ''
     sftp_enabled: bool
-    sftp_keyvaultsecretname: str
-    sftp_username: str
-    sftp_url: str
-    sftp_port: int
+    sftp_keyvaultsecretname: str = ''
+    sftp_username: str = ''
+    sftp_url: str = ''
+    sftp_port: int = 22
     var_localfolder_enabled: bool
-    var_localfolder_directory: str
+    var_localfolder_directory: str = ''
     var_unzip_directory: str
     var_sample_size: int
     var_df_chunksize: int
     var_download_directory: str
     var_loglocation: str
-    var_webhook_url: str
-    var_zipfile_prefix: str
+    var_webhook_url: str = ''
+    var_zipfile_prefix: str = ''
     var_zipfile_suffix: str
     var_location_ignorelist: str
     var_max_retry: int
@@ -84,13 +84,13 @@ class Config(BaseSettings):
     var_master_summary_filename: str
     var_verify_enabled: bool
     velociraptor_enabled: bool
-    velociraptor_url: str
-    velociraptor_remappingdir: str
-    velociraptor_binary: str
-    velociraptor_definitions: str
-    velociraptor_outputformat: str
-    velociraptor_artifactslist: str
-    velociraptor_duration: int
+    velociraptor_url: str = ''
+    velociraptor_remappingdir: str = ''
+    velociraptor_binary: str = ''
+    velociraptor_definitions: str = ''
+    velociraptor_outputformat: str = ''
+    velociraptor_artifactslist: str = ''
+    velociraptor_duration: int = 400
 
 def load_config(env_file: str = None) -> Config:
     '''
